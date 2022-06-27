e387f8a9a913afe6ad92f759da60405b8f07a19e
Gabriele <gabriele.spina99@gmail.com>
2022-06-27 19:14:48 +0200

    27/06

--- ADR/plot_bounds.py
+++ ADR/plot_bounds.py
@@ -14,7 +14,7 @@ x = parser.parse_args().by
 if x not in ['ep_num', 'timesteps']:
     exit('Wrong arguments, choose between [ep_num, timesteps]')
 
-path = './logs/train.npz'
+path = './train.npz'
 log = np.load(path)
 bounds = log['bounds']
 my_labels = ['foot', '_Hidden', 'thigh', '_Hidden', 'leg', '_Hidden']
@@ -23,8 +23,8 @@ cmap = mpl.colors.ListedColormap(['red', 'green', 'blue'])
 ax = bounds_df.plot(colormap=cmap)
 handles, _ = ax.get_legend_handles_labels()
 my_handles = [handles[i] for i in range(-2,4)]
-ax.legend(labels=my_labels, handles=my_handles, fontsize='large')
-ax.set_xlabel('timesteps' if x =='timesteps' else 'episodes')
-ax.set_ylabel('mass (kg)')
+ax.legend(labels=my_labels, handles=my_handles, fontsize='large', loc='upper right', bbox_to_anchor=(1, 0.9))
+ax.set_xlabel('timesteps' if x =='timesteps' else 'episodes', fontsize='large')
+ax.set_ylabel('mass (kg)', fontsize='large')
 plt.grid()
-plt.show()
+plt.savefig('bounds_proportional.pdf', format='pdf')
